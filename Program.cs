5db43b08bf4faeb11fa6dfe5c93ffb74b8642cf8
ADMIN <ADMIN@LAB1-MAY29>
2023-09-04 14:43:16 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace schap_bai1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; 1 < 10; i++)
+            {
+                Console.Write("hello world " + i);
+            }
+            Console.WriteLine();        
+        }
+    }
+}
